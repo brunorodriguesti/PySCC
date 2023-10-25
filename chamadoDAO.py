25ede27b5d433c0cf1aa2fee71dab245a03c28ee
brunorodriguesti <brunorodriguesti@yahoo.com.br>
2023-10-24 22:00:22 -0300

    Modificação do banco de dados e tabela

--- chamadoDAO.py
+++ chamadoDAO.py
@@ -2,9 +2,24 @@ import sqlite3
 
 class ChamadoDAO:
 
+    def __init__(self):
+        self.conexao = sqlite3.connect("SCC.db")
+        cursor = self.conexao.cursor()
+        try:
+            comando = "CREATE TABLE IF NOT EXISTS TB_CHAMADO (CODIGO INTEGER, DESCRICAO TEXT, ABERTO_POR TEXT, DATA_ABERTURA DATE, DATA_FECHAMENTO DATE, QTD_ABERT INTEGER, STATUS TEXT, PRIMARY KEY(CODIGO));"
+            cursor.execute(comando)
+            print("Banco de dados criado com sucesso")
+        except sqlite3.DatabaseError as err:
+            print("Erro ao criar o banco de dados")
+        finally:
+            if self.conexao:
+                cursor.close()
+                self.conexao.close()
+
+
     def abrirConexao(self):
         try:
-            self.conexao = sqlite3.connect("trabalho.db")
+            self.conexao = sqlite3.connect("SCC.db")
             self.cursor = self.conexao.cursor()
 
             return True
@@ -19,7 +34,7 @@ class ChamadoDAO:
 
     def buscar(self):
         if(self.abrirConexao()):
-            self.cursor.execute("select * from natureza_juridica")
+            self.cursor.execute("select * from tb_chamado")
             resultado = self.cursor.fetchall()
             self.fecharConexao() 
             return resultado
@@ -28,7 +43,7 @@ class ChamadoDAO:
         
     def buscarPorCodigo(self, codigo):
         if(self.abrirConexao()):
-            self.cursor.execute("select * from natureza_juridica where codigo = ?",(codigo,))
+            self.cursor.execute("select * from tb_chamado where codigo = ?",(codigo,))
             resultado = self.cursor.fetchone()
             self.fecharConexao() 
             return resultado
@@ -38,7 +53,7 @@ class ChamadoDAO:
     def inserir(self, codigo, descricao):
          if(self.abrirConexao()):
             try:
-                self.cursor.execute("insert into natureza_juridica (codigo, descricao) values (?,?)",(codigo,descricao))            
+                self.cursor.execute("insert into tb_chamado (codigo, descricao) values (?,?)",(codigo,descricao))            
                 self.conexao.commit()
                 self.fecharConexao()
             except sqlite3.DatabaseError as erro:
@@ -47,7 +62,7 @@ class ChamadoDAO:
     def excluir(self, codigo):
          if(self.abrirConexao()):
             try:
-                self.cursor.execute("delete from natureza_juridica where codigo = ?",(codigo,))            
+                self.cursor.execute("delete from tb_chamado where codigo = ?",(codigo,))            
                 self.conexao.commit()
                 self.fecharConexao()
             except sqlite3.DatabaseError as erro:
